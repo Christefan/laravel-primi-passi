e7cda3c100c4b4cd52cbfec8471ef2f9b72a13a8
Christefan <chriscapod7@gmail.com>
2022-06-23 15:46:11 +0200

    Laravel + Bonus

--- resources/views/option1.blade.php
+++ resources/views/option1.blade.php
@@ -1,3 +1,3 @@
 <a href="/">Home</a>
 <a href="/prova2">Option 2</a>
-<h1>{{$title}}</h1>
+<h1>{{$title}} !</h1>
--- resources/views/option2.blade.php
+++ resources/views/option2.blade.php
@@ -1,3 +1,3 @@
 <a href="/">Home</a>
 <a href="/prova1">Option 1</a>
-<h1>{{$title}}</h1>
+<h1>{{$title}} !</h1>
